4589e4a6f292b56403198e7a92868d3d1fd14daf
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-01 22:27:18 +0100

    Projects rendered to a list.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+const initialProjects = [
+  {
+    id: 1,
+    garment: "Shorts",
+    fabric: "linen",
+    started: false,
+    completed: false,
+  },
+  {
+    id: 2,
+    garment: "Black Dress",
+    fabric: "viscose",
+    started: false,
+    completed: false,
+  },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <ProjectList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>Sewing Projects</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you want to add? </h3>
+    </div>
+  );
+}
+function ProjectList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialProjects.map((project) => (
+          <Project project={project} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Project({ project }) {
+  return (
+    <li>
+      <span>
+        {project.garment} ({project.fabric})
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <em>
+      <footer className="stats">
+        You have x items in your list, and you already bought{" "}
+      </footer>
+    </em>
+  );
+}
