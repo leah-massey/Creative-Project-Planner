41f9a04e6c03c38cadcda3b61f600f998eb6d7ac
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-08 20:25:10 +0100

    Check box added for when project completed.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,5 @@
 import { useState } from "react";
 
-const initialProjects = [
-  {
-    id: 1,
-    category: "sewing",
-    item: "Shorts",
-
-    projectSize: "M",
-    started: false,
-    completed: false,
-  },
-  {
-    id: 2,
-    category: "sewing",
-    item: "Black Dress",
-
-    projectSize: "L",
-    started: false,
-    completed: false,
-  },
-
-  {
-    id: 3,
-    category: "sewing",
-    item: "Cream Skirt",
-
-    projectSize: "L",
-    started: true,
-    completed: true,
-  },
-
-  {
-    id: 4,
-    category: "art",
-    item: "Christmas cards",
-
-    projectSize: "M",
-    started: false,
-    completed: false,
-  },
-];
-
 export default function App() {
   const [projects, setProjects] = useState([]);
 
@@ -52,12 +11,26 @@ export default function App() {
     setProjects((projects) => projects.filter((project) => project.id !== id));
   }
 
+  function handleToggleProject(id) {
+    setProjects((projects) =>
+      projects.map((project) =>
+        project.id === id
+          ? { ...project, completed: !project.completed }
+          : project
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddProjects={handleAddProjects} />
       {/* it's convention to write the above line like this */}
-      <ProjectList projects={projects} onDeleteProject={handleDeleteProject} />
+      <ProjectList
+        projects={projects}
+        onDeleteProject={handleDeleteProject}
+        onToggleProject={handleToggleProject}
+      />
       <Stats />
     </div>
   );
@@ -136,7 +109,7 @@ function Form({ onAddProjects }) {
     </form>
   );
 }
-function ProjectList({ projects, onDeleteProject }) {
+function ProjectList({ projects, onDeleteProject, onToggleProject }) {
   return (
     <div className="list">
       <ul>
@@ -145,6 +118,7 @@ function ProjectList({ projects, onDeleteProject }) {
             project={project}
             onDeleteProject={onDeleteProject}
             key={project.id}
+            onToggleProject={onToggleProject}
           />
         ))}
       </ul>
@@ -152,9 +126,14 @@ function ProjectList({ projects, onDeleteProject }) {
   );
 }
 
-function Project({ project, onDeleteProject }) {
+function Project({ project, onDeleteProject, onToggleProject }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={project.completed}
+        onChange={() => onToggleProject(project.id)}
+      />
       <span style={project.completed ? { textDecoration: "line-through" } : {}}>
         {project.projectDescription}
       </span>
