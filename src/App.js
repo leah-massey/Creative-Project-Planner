55c9c9f5a7e476991f105dd04e9bf5bcb95c4e61
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-09 11:21:45 +0100

    Project status dropdown created but also not working yet. Commented out in code, to be revisited.

--- src/App.js
+++ src/App.js
@@ -110,17 +110,71 @@ function Form({ onAddProjects }) {
     </form>
   );
 }
+
 function ProjectList({ projects, onDeleteProject, onToggleProject }) {
-  const [sortBy, setSortBy] = useState("newest");
+  const [sortByType, setSortByType] = useState("allTypes");
+  const [sortBySize, setSortBySize] = useState("allSizes");
+  // const [sortByStatus, setSortByStatus] = useState("all");
+  // const [sortByDate, setSortByDate] = useState("newest");
 
   let sortedProjects;
-  // refcator using case???
-  if (sortBy === "newest") sortedProjects = projects;
-  if (sortBy === "oldest") sortedProjects = [...projects].reverse();
-  if (sortBy === "sewing")
-    sortedProjects = projects.filter(
-      (project) => project.projectType === "Sewing"
-    );
+
+  //* sorting by date not currently working 👇🏻
+  // if (sortByDate === "newest") sortedProjects = projects;
+  // if (sortByDate === "oldest") sortedProjects = projects.slice().reverse();
+
+  //* sorting by status not currently working
+  // if (sortByStatus === "all") sortedProjects = projects;
+  // if (sortByStatus === "incomplete")
+  //   sortedProjects = projects
+  //     .slice()
+  //     .sort((a, b) => Number(!a.complete) - Number(!b.complete));
+
+  // sorting by type
+  if (sortByType === "allTypes") sortedProjects = projects;
+
+  if (sortByType === "sewing")
+    sortedProjects = projects
+      .slice()
+      .filter((project) => project.projectType === "Sewing");
+
+  if (sortByType === "coding")
+    sortedProjects = projects
+      .slice()
+      .filter((project) => project.projectType === "Coding");
+  if (sortByType === "art")
+    sortedProjects = projects
+      .slice()
+      .filter((project) => project.projectType === "Art");
+  if (sortByType === "interior")
+    sortedProjects = projects
+      .slice()
+      .filter((project) => project.projectType === "Interior");
+  if (sortByType === "other")
+    sortedProjects = projects
+      .slice()
+      .filter((project) => project.projectType === "Other");
+
+  // sorting by project size
+  if (sortBySize === "xs")
+    sortedProjects = projects
+      .slice()
+      .filter((project) => project.projectSize === "XS");
+
+  if (sortBySize === "s")
+    sortedProjects = projects
+      .slice()
+      .filter((project) => project.projectSize === "S");
+
+  if (sortBySize === "m")
+    sortedProjects = projects
+      .slice()
+      .filter((project) => project.projectSize === "M");
+
+  if (sortBySize === "l")
+    sortedProjects = projects
+      .slice()
+      .filter((project) => project.projectSize === "L");
 
   return (
     <div className="list">
@@ -134,17 +188,68 @@ function ProjectList({ projects, onDeleteProject, onToggleProject }) {
           />
         ))}
       </ul>
+
+      {/*//* sort by date not currently working*/}
+      {/* <div className="actions">
+        <p>
+          by date
+          <select
+            value={sortByDate}
+            onChange={(e) => setSortByDate(e.target.value)}
+          >
+            <option value="newest">Newest</option>
+            <option value="oldest">Oldest</option>
+          </select>
+        </p>
+      </div> */}
+
+      {/* //* sort by status not currenlty working*/}
+      {/* <div className="actions">
+        <p>
+          project status
+          <select
+            value={sortByType}
+            onChange={(e) => setSortByStatus(e.target.value)}
+          >
+            <option value="all">All</option>
+            <option value="incomplete">incomplete</option>
+          </select>
+        </p>
+      </div> */}
+
+      {/* sort by type */}
       <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="size">Sort by project size</option>
-          <option value="oldest">Sort by oldest</option>
-          <option value="newest">Sort by newest</option>
-          <option value="sewing">View only Sewing projects</option>
-          <option value="coding">View only Coding projects</option>
-          <option value="art">View only Art projects</option>
-          <option value="interior">View only Interior projects</option>
-          <option value="other">View only Other projects</option>
-        </select>
+        <p>
+          project type
+          <select
+            value={sortByType}
+            onChange={(e) => setSortByType(e.target.value)}
+          >
+            <option value="allTypes">All</option>
+            <option value="sewing">Sewing</option>
+            <option value="coding">Coding</option>
+            <option value="art">Art</option>
+            <option value="interior">Interior</option>
+            <option value="other">Other</option>
+          </select>
+        </p>
+      </div>
+
+      {/* sorting by size */}
+      <div className="actions">
+        <p>
+          size
+          <select
+            value={sortBySize}
+            onChange={(e) => setSortBySize(e.target.value)}
+          >
+            <option value="allSizes">All</option>
+            <option value="xs">XS</option>
+            <option value="s">S</option>
+            <option value="m">M</option>
+            <option value="l">L</option>
+          </select>
+        </p>
       </div>
     </div>
   );
